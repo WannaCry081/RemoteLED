721e9a222c9dc61c7550a489dc6849a2508812d8
Lirae Que Data <liraedata59@gmail.com>
2024-03-09 13:45:22 +0800

    Add getLightStatus method in main.cpp

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,12 +1,18 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
+#include <HTTPClient.h>
 #include "ESPWifi.hpp"
 #define LED_PIN 12
 
 
 const String ssid = "waraykawifi";
 const String password = "KaRAOTMO##!";
+bool status = false;
+
 
 ESPWifi wifi(ssid, password);
+
+void sendGETRequest();
 
 void setup(){
 
@@ -34,4 +40,35 @@ void loop(){
   if (wifi.isConnect()){
     digitalWrite(LED_PIN, HIGH);
   }
+}
+
+
+void getLightStatus() {
+  HTTPClient http;
+
+  // Your server endpoint for GET request
+  String url = "http://192.168.100.24:8000/api/v1/light/1/";
+
+  // Send GET request
+  http.begin(url);
+  int httpCode = http.GET();
+
+  if (httpCode > 0) {
+    String payload = http.getString();
+
+    // Parse the JSON response
+    const size_t capacity = JSON_OBJECT_SIZE(1) + 20; // Adjust the size as needed
+    DynamicJsonDocument jsonDocument(capacity);
+
+    deserializeJson(jsonDocument, payload);
+
+    // Extract the "led" value
+    status = jsonDocument["led"];
+
+    Serial.println("GET Response: " + payload);
+  } else {
+    Serial.println("GET request failed");
+  }
+
+  http.end();
 }
