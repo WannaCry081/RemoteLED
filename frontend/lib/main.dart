f84a89cb550cd5ba635f20ba0fcf14e0df0f6d19
Lirae Que Data <liraedata59@gmail.com>
2024-03-09 16:22:43 +0800

    Add dotenv loader in main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,10 @@
 import "package:flutter/material.dart";
 import "package:frontend/src/pages/home/index.dart";
+import "package:flutter_dotenv/flutter_dotenv.dart";
 
 
-void main(){
+Future<void> main() async{
+  await dotenv.load();
   runApp(const MyApp());
 }
 
