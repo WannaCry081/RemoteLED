8bb43b156954d1989944641ab3a6294efa2e3901
Lirae Que Data <liraedata59@gmail.com>
2024-03-09 13:41:20 +0800

    Add ESPWifi in main.cpp

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
+#include "ESPWifi.hpp"
 #define LED_PIN 12
+
+
+const String ssid = "waraykawifi";
+const String password = "KaRAOTMO##!";
+
 
 void setup(){
 
   Serial.begin(115200);
+  ESPWifi wifi(ssid, password);
+
+  Serial.printf("Connecting to %s...", ssid);
+  while (!wifi.isConnect()) {
+    Serial.print(".");
+    delay(1000);
+
+    if (!wifi.isSuccess()) {
+      Serial.println("\nError connecting to Wifi...");
+    }
+  }
 
   pinMode(LED_PIN, OUTPUT);
 } 
