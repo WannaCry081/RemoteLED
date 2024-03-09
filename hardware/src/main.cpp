91b796712a66adc32d9c1c1b329859ec63075feb
Lirae Que Data <liraedata59@gmail.com>
2024-03-09 13:14:39 +0800

    Add blinking light in main.cpp

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
+#define LED_PIN 12
 
-// put function declarations here:
-int myFunction(int, int);
+void setup(){
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  Serial.begin(115200);
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+  pinMode(LED_PIN, OUTPUT);
+} 
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+void loop(){
+
+
+  digitalWrite(LED_PIN, HIGH);
+
 }
